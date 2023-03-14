6e9a3de8599213141728463c47b86d7f26b056fc
LineCoran <vppisya@gmail.com>
2023-03-14 19:20:53 +0300

    feat: add react-router-dom, create 404, main and abut pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./pages/About/About";
+import Main from "./pages/Main/Main";
+import PageNotFound from "./pages/PageNotFound/PageNotFound";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+  },
+  {
+    path: "about",
+    element: <About />,
+  },
+  {
+    path: "*",
+    element: <PageNotFound />,
+  },
+]);
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
