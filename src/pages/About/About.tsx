268fc365a31d2bd1f740cbc39c5a28260d804279
LineCoran <vppisya@gmail.com>
2023-03-18 19:34:51 +0300

    refactor: styled title in 404 and about pages

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import NavBar from '../../components/NavBar/NavBar';
 import Container from '../../components/Container/Container';
+import './About.css';
 
 export default class About extends Component {
   render() {
     return (
       <Container>
         <NavBar />
-        <h1 data-testid="testTitleId">Hello its about Page</h1>
+        <h1 className="aboutTitle" data-testid="testTitleId">
+          Hello its about Page
+        </h1>
       </Container>
     );
   }
--- src/pages/PageNotFound/PageNotFound.tsx
+++ src/pages/PageNotFound/PageNotFound.tsx
@@ -1,14 +1,15 @@
-import React, { Component } from 'react';
-import NavBar from '../../components/NavBar/NavBar';
+import { Component } from 'react';
 import Container from '../../components/Container/Container';
 import { Link } from 'react-router-dom';
+import './PageNotFound.css';
 
 export default class About extends Component {
   render() {
     return (
       <Container>
-        <NavBar />
-        <h1 data-testid="titleTestId">NOT FOUND PAGE</h1>
+        <h1 className="title" data-testid="titleTestId">
+          NOT FOUND PAGE
+        </h1>
         <Link to="/">Home</Link>
       </Container>
     );
